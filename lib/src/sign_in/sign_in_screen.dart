85de82ec98d9c10aa015813c7c9f0548ec697246
Matias Fare <matiasfare59@gmail.com>
2021-02-19 22:53:38 -0300

    Agrega pantalla de Login

---
+++ lib/src/sign_in/sign_in_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:firulapp/src/size_config.dart';
+import 'components/body.dart';
+
+class SignInScreen extends StatelessWidget {
+  static String routeName = "/sign_in";
+  @override
+  Widget build(BuildContext context) {
+    // You have to call it on your starting screen
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign In"),
+      ),
+      body: Body(),
+    );
+  }
+}
