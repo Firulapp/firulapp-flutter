a8d71c6e798da7facab31892d4af7de0cf3ec571
Matias Fare <matiasfare59@gmail.com>
2021-02-19 22:53:06 -0300

    Agrega mixins para validación de credenciales

---
+++ lib/src/mixins/validator_mixins.dart
@@ -0,0 +1,22 @@
+import 'package:firulapp/src/constants.dart';
+
+class ValidatorMixins {
+  String validateEmail(String value) {
+    if (!value.contains('@') | !emailValidatorRegExp.hasMatch(value)) {
+      return kInvalidEmailError;
+    } else if (value.isEmpty) {
+      return kEmailNullError;
+    }
+    return null;
+  }
+
+  String validatePassword(String value) {
+    if (value.isEmpty) {
+      return kPassNullError;
+    } else if (value.length < 8) {
+      print(value.length);
+      return kShortPassError;
+    }
+    return null;
+  }
+}
