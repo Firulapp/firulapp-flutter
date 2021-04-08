7bb53269aeecafee9231fed7cede0e859ed46aeb
Javier Heisecke <javierheiseckee@gmail.com>
2021-04-07 22:07:18 -0400

    Agrega estado de menu intermedio para mascotas

---
+++ lib/src/pets/selected_pet_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../provider/pets.dart';
+import '../profile/components/profile_menu.dart';
+import '../profile/components/profile_pic.dart';
+
+class SelectedPetScreen extends StatelessWidget {
+  static const routeName = "/selected-pet";
+
+  @override
+  Widget build(BuildContext context) {
+    final id = ModalRoute.of(context).settings.arguments as int;
+    final pet = Provider.of<Pets>(context, listen: false).getLocalPetById(id);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(pet.name),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: Column(children: [
+          ProfilePic(),
+          SizedBox(height: 20),
+          ProfileMenu(
+            text: "Mi Mascota",
+            icon: "assets/icons/User Icon.svg",
+            press: () => {
+              //Navigator.pushNamed(context, ProfilePage.routeName),
+              null
+            },
+          ),
+          ProfileMenu(
+            text: "Fichas Médicas",
+            icon: "assets/icons/Bell.svg",
+            press: () {
+              //Navigator.pushNamed(context,MedicalRecordsScreen.routeName,arguments: pet,);
+            },
+          ),
+          ProfileMenu(
+            text: "Libreta de Vacunación",
+            icon: "assets/icons/Settings.svg",
+            press: () {},
+          ),
+        ]),
+      ),
+    );
+  }
+}
