aa0c1e316cce77479c2864dd063e846a6390041c
Javier Heisecke <javierheiseckee@gmail.com>
2021-04-13 23:34:00 -0400

    Agrega Item de vacunas

---
+++ lib/src/vaccionation_records/components/vaccination_record_item.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../../../provider/vaccination_record.dart' as vaxProvider;
+
+class VaccinationRecordItem extends StatelessWidget {
+  final vaxProvider.VaccinationRecordItem _vaxRecords;
+  const VaccinationRecordItem(
+    this._vaxRecords, {
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Card(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(color: Colors.white70, width: 1),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        margin: EdgeInsets.all(10),
+        elevation: 10,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Padding(padding: EdgeInsets.symmetric(vertical: 2.0)),
+            Text(
+              "Consulta: ${_vaxRecords.vaccinationDate}",
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            const Padding(padding: EdgeInsets.symmetric(vertical: 2.0)),
+            Text(
+              "Veterinaria: ${_vaxRecords.veterinary}",
+              style: const TextStyle(fontSize: 20.0),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Vacuna: ${_vaxRecords.vaccine}',
+                style: const TextStyle(fontSize: 16.0),
+              ),
+            ),
+          ],
+        ),
+      ),
+      onTap: () {
+        /*Navigator.of(context).pushNamed(
+          NewMedicalRecordScreen.routeName,
+          arguments: _vaxRecords.id,
+        );*/
+      },
+    );
+  }
+}
