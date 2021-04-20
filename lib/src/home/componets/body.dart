933cfbb3c4e3db04377b810262cf0af1354cc621
Javier Heisecke <javierheiseckee@gmail.com>
2021-04-20 01:09:08 -0400

    Ajusta menu en el home

--- lib/src/home/componets/body.dart
+++ lib/src/home/componets/body.dart
@@ -1,13 +1,81 @@
 import 'package:flutter/material.dart';
 
-import 'home_buttons.dart';
+import '../../../components/menu_button.dart';
+import '../../../size_config.dart';
+import '../../pets/pets_screen.dart';
+import '../../../constants/constants.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final SizeConfig sizeConfig = SizeConfig();
     return SafeArea(
       child: Center(
-        child: HomeButtons(),
+        child: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: sizeConfig.wp(4.5),
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(bottom: sizeConfig.wp(9)),
+                    child: Text(
+                      'Firulapp',
+                      style: TextStyle(
+                        fontSize: sizeConfig.hp(4),
+                        color: kSecondaryColor,
+                      ),
+                    ),
+                  ),
+                  MenuButton(
+                    sizeConfig: sizeConfig,
+                    label: 'Mascotas',
+                    icon: Icons.pets_rounded,
+                    routeName: "",
+                    color: kPrimaryColor,
+                  ),
+                  SizedBox(
+                    height: getProportionateScreenHeight(25),
+                  ),
+                  MenuButton(
+                    sizeConfig: sizeConfig,
+                    label: 'Servicios',
+                    icon: Icons.store,
+                    routeName: "",
+                    color: kPrimaryColor,
+                  ),
+                  SizedBox(
+                    height: getProportionateScreenHeight(25),
+                  ),
+                  MenuButton(
+                    sizeConfig: sizeConfig,
+                    label: 'Cuidados',
+                    icon: Icons.favorite,
+                    routeName: "",
+                    color: kPrimaryColor,
+                  ),
+                  SizedBox(
+                    height: getProportionateScreenHeight(25),
+                  ),
+                  MenuButton(
+                    sizeConfig: sizeConfig,
+                    label: 'Agenda',
+                    icon: Icons.calendar_today,
+                    routeName: "",
+                    color: kPrimaryColor,
+                  ),
+                  SizedBox(
+                    height: getProportionateScreenHeight(25),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
