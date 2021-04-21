f5c7c0b3db4cc0651b1708bb984b4869f558ef94
Javier Heisecke <javierheiseckee@gmail.com>
2021-04-20 22:22:57 -0400

    Ajusta la agenda con datos de prueba

--- lib/src/agenda/agenda_screen.dart
+++ lib/src/agenda/agenda_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firulapp/constants/constants.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:flutter/material.dart';
 
@@ -10,11 +11,33 @@ class AgendaScreen extends StatefulWidget {
 
 class _AgendaScreenState extends State<AgendaScreen> {
   var _calendarController;
+  Map<DateTime, List<dynamic>> _events;
+  List<dynamic> _selectedEvents;
+  TextEditingController _eventController;
 
   @override
   void initState() {
     super.initState();
     _calendarController = CalendarController();
+    _eventController = TextEditingController();
+    _events = {};
+    _selectedEvents = [];
+  }
+
+  Map<String, dynamic> encodeMap(Map<DateTime, dynamic> map) {
+    Map<String, dynamic> newMap = {};
+    map.forEach((key, value) {
+      newMap[key.toString()] = map[key];
+    });
+    return newMap;
+  }
+
+  Map<DateTime, dynamic> decodeMap(Map<String, dynamic> map) {
+    Map<DateTime, dynamic> newMap = {};
+    map.forEach((key, value) {
+      newMap[DateTime.parse(key)] = map[key];
+    });
+    return newMap;
   }
 
   @override
@@ -29,8 +52,96 @@ class _AgendaScreenState extends State<AgendaScreen> {
       appBar: AppBar(
         title: const Text("Agenda"),
       ),
-      body: TableCalendar(
-        calendarController: _calendarController,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        child: Icon(Icons.add),
+        onPressed: _showAddDialog,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            TableCalendar(
+              events: _events,
+              initialCalendarFormat: CalendarFormat.month,
+              calendarController: _calendarController,
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              calendarStyle: CalendarStyle(
+                canEventMarkersOverflow: true,
+                todayColor: kPrimaryLightColor,
+                selectedColor: kPrimaryColor,
+                todayStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                  color: Colors.white,
+                ),
+              ),
+              onDaySelected: (date, events, holidays) {
+                setState(() {
+                  _selectedEvents = events;
+                });
+              },
+            ),
+            ..._selectedEvents.map(
+              (event) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: MediaQuery.of(context).size.height / 20,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                      border: Border.all(color: Colors.grey)),
+                  child: Center(
+                    child: Text(
+                      event,
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _showAddDialog() async {
+    await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: Colors.white70,
+        title: Text("Add Events"),
+        content: TextField(
+          controller: _eventController,
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text(
+              "Save",
+              style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+            ),
+            onPressed: () {
+              if (_eventController.text.isEmpty) return;
+              setState(() {
+                if (_events[_calendarController.selectedDay] != null) {
+                  _events[_calendarController.selectedDay]
+                      .add(_eventController.text);
+                } else {
+                  _events[_calendarController.selectedDay] = [
+                    _eventController.text
+                  ];
+                }
+                _eventController.clear();
+                Navigator.pop(context);
+              });
+            },
+          )
+        ],
       ),
     );
   }
