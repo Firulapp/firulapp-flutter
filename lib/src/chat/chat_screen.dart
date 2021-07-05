e44c00f5bb6ad265affaf0f5062401d233c0cb98
Matias Fare <matiasfare59@gmail.com>
2021-07-04 23:42:50 -0400

    Conecta app con database firebase

--- lib/src/chat/chat_screen.dart
+++ lib/src/chat/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -12,6 +13,19 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(8),
           child: Text('Chat funciona'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chat/Th6J80GzZ3iu4kKU4oLj/messages/')
+              .snapshots()
+              .listen(
+            (data) {
+              print(data.documents[0]['text']);
+            },
+          );
+        },
       ),
     );
   }
